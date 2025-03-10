44bcd9efcfa9049a96849c77e4bcb3ffcf549bc9
Patrick John <paerana@up.edu.ph>
2025-03-10 15:54:16 +0800

    main.py

--- main.py
+++ main.py
@@ -6,7 +6,45 @@ from session import SessionLayer
 from presentation import PresentationLayer  
 from application import ApplicationLayer  
 
-# create instances of all layers  
+#dynamic input from user
+print("\n--- OSI Simulation Setup ---")
+sender_mac = input("Enter sender MAC address (e.g., AA:BB:CC:DD:EE:FF): ")
+sender_ip = input("Enter sender IP address (e.g., 192.168.1.1): ")
+user_message = input("Enter a message to send: ")
+
+#initialize layers 
+physical = PhysicalLayer()
+data_link = DataLinkLayer(mac_address=sender_mac)
+network = NetworkLayer(ip_address=sender_ip)
+transport = TransportLayer()
+session = SessionLayer()
+presentation = PresentationLayer()
+application = ApplicationLayer()
+
+#simulate sending data
+print("\n--- Sending Data ---\n")
+session.start_session()
+app_data = application.send_request(user_message)
+pres_data = presentation.encapsulate(app_data)
+sess_data = session.encapsulate(pres_data)
+trans_data = transport.encapsulate(sess_data)
+net_data = network.encapsulate(trans_data)
+dl_data = data_link.encapsulate(net_data)
+phy_data = physical.transmit(dl_data)
+
+#simulate receiving data
+print("\n--- Receiving Data ---\n")
+dl_received = physical.receive(phy_data)
+net_received = data_link.decapsulate(dl_received)
+trans_received = network.decapsulate(net_received)
+sess_received = transport.decapsulate(trans_received)
+pres_received = session.decapsulate(sess_received)
+app_received = presentation.decapsulate(pres_received)
+response = application.receive_response(app_received)
+session.end_session()
+
+""" Static ver
+#create instances of all layers  
 physical = PhysicalLayer()  
 data_link = DataLinkLayer()  
 network = NetworkLayer()  
@@ -34,4 +72,4 @@ sess_received = transport.decapsulate(trans_received)
 pres_received = session.decapsulate(sess_received)  
 app_received = presentation.decapsulate(pres_received)  
 response = application.receive_response(app_received)  
-session.end_session()  
+session.end_session()  """
